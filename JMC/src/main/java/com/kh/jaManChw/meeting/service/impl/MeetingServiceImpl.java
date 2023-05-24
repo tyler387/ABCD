15e0e344b10ce91e2f63d61f48c2158595023294
jinhwan <tyler272@naver.com>
2023-05-24 17:29:36 +0900

    JINHWAN 2023-05-24 PM 05:29

--- JMC/src/main/java/com/kh/jaManChw/meeting/service/impl/MeetingServiceImpl.java
+++ JMC/src/main/java/com/kh/jaManChw/meeting/service/impl/MeetingServiceImpl.java
@@ -1,11 +1,15 @@
 package com.kh.jaManChw.meeting.service.impl;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.kh.jaManChw.dto.FriendList;
 import com.kh.jaManChw.dto.Meeting;
+import com.kh.jaManChw.dto.Users;
 import com.kh.jaManChw.meeting.controller.MeetingController;
 import com.kh.jaManChw.meeting.dao.face.MeetingDao;
 import com.kh.jaManChw.meeting.service.face.MeetingService;
@@ -23,6 +27,12 @@ private final Logger logger = LoggerFactory.getLogger(MeetingController.class);
 	public void inputMeeting(Meeting meeting) {
 		
 		meetingDao.insertMeeting(meeting);
+			
+	}
+	
+	@Override
+	public List<Users> selectFriendListAll() {
 		
+		return meetingDao.selectFriendListAll();
 	}
 }

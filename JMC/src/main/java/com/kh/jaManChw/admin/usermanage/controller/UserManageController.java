80078bc8dc8535fd345f696bbd9453ca7a4d9a0e
seokbong <skfkqhd@gmail.com>
2023-05-24 11:38:39 +0900

    2023/05/24 commit

--- JMC/src/main/java/com/kh/jaManChw/admin/usermanage/controller/UserManageController.java
+++ JMC/src/main/java/com/kh/jaManChw/admin/usermanage/controller/UserManageController.java
@@ -1,9 +1,15 @@
 package com.kh.jaManChw.admin.usermanage.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.jaManChw.admin.usermanage.service.face.UserManageService;
+import com.kh.jaManChw.dto.Users;
 
 @RequestMapping("/admin/user")
 @Controller
@@ -12,9 +18,14 @@ public class UserManageController {
 	//log4j.xml에서 <logger> 설정 필요
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
+	@Autowired UserManageService userManageService;
+	
 	@RequestMapping("/mg")
 	public void user() {
 		logger.info("abc");
+		
+		List<Users> users = userManageService.UserMgPage();
+		logger.info("{}", users);
 	}
 	
 	
--- JMC/src/main/java/com/kh/jaManChw/admin/usermanage/dao/face/UserManageDao.java
+++ JMC/src/main/java/com/kh/jaManChw/admin/usermanage/dao/face/UserManageDao.java
@@ -1,5 +1,11 @@
 package com.kh.jaManChw.admin.usermanage.dao.face;
 
+import java.util.List;
+
+import com.kh.jaManChw.dto.Users;
+
 public interface UserManageDao {
+
+	public List<Users> selectUserMgPage();
 
 }

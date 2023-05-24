aff5addca427d4ba53b36d0f37c855264572cf29
seokbong <skfkqhd@gmail.com>
2023-05-24 10:28:42 +0900

    2023/05/24 commit

--- JMC/src/main/java/com/kh/jaManChw/admin/usermanage/controller/UserManageController.java
+++ JMC/src/main/java/com/kh/jaManChw/admin/usermanage/controller/UserManageController.java
@@ -3,12 +3,19 @@ package com.kh.jaManChw.admin.usermanage.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/admin/user")
 @Controller
 public class UserManageController {
 	
 	//log4j.xml에서 <logger> 설정 필요
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-
+	@RequestMapping("/mg")
+	public void user() {
+		logger.info("abc");
+	}
+	
+	
 }

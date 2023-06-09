dfbb7ef4e9541d7e343c3fe9d0d2a830edc7e7cb
Dong <developdong@gmail.com>
2023-06-09 14:50:22 +0900

    FEAT: 인터셉터 컨트롤러 추가
    인터셉터 기능 추가 중 아직 수정할 부분 많음

--- JMC/src/main/java/com/kh/jaManChw/util/interceptor/AdminInterceptor.java
+++ JMC/src/main/java/com/kh/jaManChw/util/interceptor/AdminInterceptor.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-public class adminInterceptor implements HandlerInterceptor{
+public class AdminInterceptor implements HandlerInterceptor{
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
@@ -27,27 +27,47 @@ public class adminInterceptor implements HandlerInterceptor{
 		if (session.getAttribute("isLogin") == null) {
 			
 			//1. 비로그인 상태
-			logger.info("접속 불가 =>> 비로그인 상태 ");
+			logger.info("접속 불가 =>> 비로그인 상태입니다, 세션을 확인하세요");
 			
+			//1-1. 비로그인시 로그인 페이지로 이동시킨다
+			response.sendRedirect("./login");
+			
+			//1-2. 컨트롤러 접근 차단
+			return false; //컨트롤러 접근 차단
+
 		}else if( session.getAttribute("role") != "admin") {
 			
 			//2. 접속자가 관리자가 아닌 경우
-			logger.info("접속 불가 =>> 관리자가 아닙니다");
+			logger.info("접속 불가 =>> 관리자가 아닙니다, 아이디 권한을 확인하세요");
 			
+			//2-1. 관리자가 아닌 경우 유저 메인페이지로 이동시킨다
+			response.sendRedirect("./main");
+			
+			//2-2. 컨트롤러 접근 차단
+			return false; //컨트롤러 접근 차단
+
 		}else if(session.getAttribute("status") != "active") {
 			
-			//3. 접속자가 관리자가 아닌 경우
-			logger.info("접속 불가 =>> 관리자가 아닙니다");
+			//3. 접속 상태가 "active"가 아닌 경우
+			logger.info("접속 불가 =>> 아이디가 활성화 상태가 아닙니다, 아이디 상태를 확인하세요");
 			
+			//3-1. 접속 상태가 "active"가 아닌 경우, 메인 페이지로 이동시킨다.
+			response.sendRedirect("./main");
+
+			//3-2. 컨트롤러 접근 차단
+			return false; //컨트롤러 접근 차단
 		}
 		
-		return false;
+		logger.info(">> 접속 허가 : 관리자 로그인 상태");
+		return true; //컨트롤러 접근 허용
+		
 	}
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		// TODO Auto-generated method stub
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		
+			logger.info(" + + + 관리자 인터셉터 종료 + + +");
+	
 	}
 }

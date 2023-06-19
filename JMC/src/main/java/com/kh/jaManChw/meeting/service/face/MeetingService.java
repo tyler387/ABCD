935b87b2705675d644bf85052a94ae5d911abc73
jinhwan <tyler272@naver.com>
2023-06-19 17:17:25 +0900

    2023-06-19 17:16 JINHWAN COMMIT 모임 신청 횟수 제한 구현 및 자기가 등록한 모임에 신고 및 신청불가구현

--- JMC/src/main/java/com/kh/jaManChw/meeting/service/face/MeetingService.java
+++ JMC/src/main/java/com/kh/jaManChw/meeting/service/face/MeetingService.java
@@ -14,30 +14,98 @@ import com.kh.jaManChw.util.MeetingPaging;
 
 public interface MeetingService {
 	
+	/**
+	 * 유저정보 가져오기 
+	 * @param userno 
+	 * @return  
+	 * 
+	 */
 	public int getUserno(int userno);
-
+	
+	/**
+	 * 모임 등록하기   
+	 * @param meeting
+	 * @param preference
+	 * @param applicant
+	 */
 	public void inputMeeting(Meeting meeting, Preference preference, Applicant applicant);
 	
+	/**
+	 * 전체 모임 조회하기 
+	 * @param meeting 모임 정보 
+	 * @return 전체 모임 목록 
+	 */
 	public List<Meeting> getMeetinglistAll(Meeting meeting);
 	
+	/**
+	 * 등록된 모임 개수 조회하기  
+	 * @param meeting 모임 정보 
+	 * @return 전체 모임 개수 
+	 */
 	public int getMeetinglistcount(Meeting meeting);
 	
+	/**
+	 * 등록된 모집중인 모임 조회하기  
+	 * @param meeting 모임 정보 
+	 * @return 모집중인 모임 개수 
+	 */
 	public int getMeetinglistcountnow(Meeting meeting);
 	
+	/**
+	 * 선택한 날짜에 맞는 모임 조회하기  
+	 * @param result 선택한 날짜 
+	 * @return 날짜에 맞는 모임 목록 
+	 */
 	public List<Meeting> getMeetingListByDate(String result);
 	
+	/**
+	 * 선택한 위치에 맞는 모임 조회하기 
+	 * @param mapData 선택한 도로명 주소 
+	 * @param mapData1 선택한 상세 주소 
+	 * @return 위치에 맞는 모임 목록 
+	 */
 	public List<Meeting> getMeetingListByMap(String mapData, String mapData1);
 	
+	/**
+	 * 필터에 맞는 모임 목록 조회하기 
+	 * @param map 필터로 선택한 인원수, 요금 
+	 * @return 필터에 맞는 모임 목록 
+	 */
 	public List<Meeting> getMeetingListByFilter(Map<String, Object> map);
 	
+	/**
+	 * 검색한 단어에 맞는 제목의 모임 목록 조회하기  
+	 * @param search 검색내용 
+	 * @return 검색한 단어에 맞는 제목의 모임 목록 
+	 */
 	public List<Meeting> getMeetingListByMname(String search);
 	
+	/**
+	 * 모임의 신청자 수 조회하기  
+	 * @param applicant 신청자 정보 
+	 * @return 신청자 수 
+	 */
 	public int getApplicantCount(Applicant applicant);
 	
+	/**
+	 * 모임에 확정된 신청자 수 조회하기  
+	 * @param applicant 신청자 정보 
+	 * @return 확정된 신청자 수 
+	 */
 	public int getApplicantCheckCount(Applicant applicant);
 
+	/**
+	 * 모임에 확정되지 않은 신청자 수 조회하기 
+	 * @param applicant 신청자 정보 
+	 * @return 확정되지 않은 신청자 수 
+	 */
 	public int getApplicantNoCheckCount(Applicant applicant);
 	
+	/** 
+	 * 선택한 모임 조회하기   
+	 * @param meeting 모임의 정보  
+	 * @return 선택한 모임 정보 
+	 */
 	public Meeting getDetailMeeting(Meeting meeting);
 	
 	public Preference getDetailPreference(Preference preference);
